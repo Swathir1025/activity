ff5b575d8260c463a4061a1a721ac067131f460c
Dell <Dell@DESKTOP-MT0J7JO>
2024-03-12 12:08:22 +0530

    first project

---
+++ activity/src/sample/UserName.java
@@ -0,0 +1,16 @@
+package sample;
+
+import java.util.Scanner;
+
+public class UserName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter your name");
+		String n=input.nextLine();
+		System.out.println("Hello"+n);
+
+	}
+
+}
